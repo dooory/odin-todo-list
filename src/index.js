266e45c3c15a117982a3fafe93d8c12d3a6d8185
Dory <78720364+dooory@users.noreply.github.com>
2026-03-31 11:00:55 +1300

    refactoring index.js to improve consistency

--- src/index.js
+++ src/index.js
@@ -1,39 +1,16 @@
+import "./style.css";
+
 import TaskInterface from "./interfaces/tasks";
 import TagInterface from "./interfaces/tags";
+import renderer from "./interfaces/renderer";
+
 import {
     Dropdown,
     refreshAllDropdowns,
     getDropdowns,
 } from "./components/dropdown";
-import "./style.css";
-import renderer from "./interfaces/renderer";
+
 import { add } from "date-fns";
-
-if (localStorage.getItem("tags")) {
-    TagInterface.loadSavedTags();
-} else {
-    TagInterface.createTag("Example Tag");
-}
-
-if (localStorage.getItem("tasks")) {
-    TaskInterface.loadSavedTasks();
-} else {
-    let myTask = TaskInterface.createTask(
-        "Example task",
-        "Example description",
-        add(new Date(), {
-            days: 1,
-        }),
-        1,
-    );
-
-    myTask.addTag(TagInterface.tags[0].id);
-}
-
-TagInterface.saveTags();
-TaskInterface.saveTasks();
-
-renderer.createAllTaskElements();
 
 const taskGroups = document.getElementById("task-groups");
 const addTaskDialog = document.getElementById("add-task-dialog");
@@ -46,9 +23,26 @@ const showAddTaskButton = document.getElementById("add-task-button");
 const submitNewTaskButton = document.getElementById("submitNewTask");
 const taskDateInput = document.getElementById("taskDate");
 
-taskDateInput.addEventListener("click", (e) => {
-    taskDateInput.showPicker();
-});
+function loadDefaultTags() {
+    TagInterface.createTag("Example Tag");
+}
+
+function loadDefaultTasks() {
+    let myTask = TaskInterface.createTask(
+        "Example task",
+        "Example description",
+        add(new Date(), {
+            days: 1,
+        }),
+        1,
+    );
+
+    myTask.addTag(TagInterface.tags[0].id);
+}
+
+function loadUI() {
+    renderer.createAllTaskElements();
+}
 
 function resetForm() {
     const inputs = addTaskForm.querySelectorAll("input, textarea, select");
@@ -128,7 +122,7 @@ function submitNewTask() {
     renderer.createTaskElementInDom(task);
 }
 
-submitNewTaskButton.addEventListener("click", (e) => {
+submitNewTaskButton.addEventListener("click", () => {
     if (addTaskForm.checkValidity()) {
         submitNewTask();
 
@@ -166,12 +160,12 @@ document.addEventListener("click", (event) => {
     }
 });
 
-createTagDialog.addEventListener("close", (event) => {
+createTagDialog.addEventListener("close", () => {
     createTagForm.reset();
 });
 
-createTagForm.addEventListener("submit", (e) => {
-    e.preventDefault();
+createTagForm.addEventListener("submit", (event) => {
+    event.preventDefault();
     const formData = new FormData(createTagForm);
 
     const newTagTitle = formData.get("new-tag-title");
@@ -198,4 +192,8 @@ createTagForm.addEventListener("submit", (e) => {
 
     createTagForm.reset();
     createTagDialog.close();
+});
+
+taskDateInput.addEventListener("click", () => {
+    taskDateInput.showPicker();
 });
