c2a498a55a51546cb7e9c10f1b5d6f6a90d4cdf3
Dory <78720364+dooory@users.noreply.github.com>
2026-04-13 15:56:31 +1200

    refactor index.js into sections

--- src/index.js
+++ src/index.js
@@ -30,114 +30,6 @@ const submitNewTaskButton = document.getElementById("submitNewTask");
 const taskDateInput = document.getElementById("taskDate");
 const closeAddTaskButton = document.getElementById("closeAddTaskDialog");
 
-function loadDefaultTags() {
-    TagInterface.createTag("Example Tag");
-}
-
-function loadDefaultTasks() {
-    let myTask = TaskInterface.createTask(
-        "Example task",
-        "Example description",
-        add(new Date(), {
-            days: 1,
-        }),
-        1,
-    );
-
-    myTask.addTag(TagInterface.tags[0].id);
-}
-
-function loadUI() {
-    renderer.createAllTaskElements();
-}
-
-function resetForm() {
-    const inputs = addTaskForm.querySelectorAll("input, textarea, select");
-
-    inputs.forEach((input) => {
-        input.disabled = true;
-    });
-
-    resetDropdown();
-
-    addTaskForm.reset();
-}
-
-let addTaskDropdown;
-
-function resetDropdown() {
-    if (addTaskDropdown) {
-        addTaskDropdown.remove();
-        addTaskDropdown = null;
-    }
-}
-
-function closeForm() {
-    showAddTaskButton.classList.remove("activated");
-    addTaskDialog.classList.remove("activated");
-}
-
-function showForm() {
-    addTaskDropdown = new Dropdown({
-        events: {
-            onCreateEntryClick: () => {
-                tagsManager.showModal();
-                toggleCreateTagOption(true);
-            },
-        },
-        getEntryData: () => {
-            const currentTags = TagInterface.tags;
-
-            return currentTags.map((tag) => {
-                let data = {};
-
-                data.title = tag.title;
-                data.id = tag.id;
-
-                return data;
-            });
-        },
-        updateOnCreate: true,
-        type: "tag",
-        selectedEntriesPlaceholder: "No tags...",
-        ignoredElements: [tagsManager],
-    });
-
-    addTaskForm.insertBefore(
-        addTaskDropdown.container,
-        addTaskForm.lastElementChild,
-    );
-
-    const inputs = addTaskForm.querySelectorAll("input, textarea, select");
-
-    inputs.forEach((input) => {
-        input.disabled = false;
-    });
-
-    showAddTaskButton.classList.add("activated");
-    addTaskDialog.classList.add("activated");
-}
-
-function submitNewTask() {
-    const data = new FormData(addTaskForm);
-    const title = data.get("task-title");
-    const dueDate = data.get("task-date");
-    const priority = data.get("task-priority");
-
-    const task = TaskInterface.createTask(
-        title,
-        "Description",
-        new Date(dueDate),
-        Number(priority),
-    );
-
-    addTaskDropdown.selectedEntries.forEach((id) => {
-        task.addTag(id);
-    });
-
-    renderer.createTaskElementInDom(task);
-}
-
 // Search/Filtering tasks handling
 
 filterForm.addEventListener("submit", (event) => event.preventDefault());
@@ -228,6 +120,92 @@ tagsManager.addEventListener("close", () => {
 });
 
 // Create new task form handling
+let addTaskDropdown;
+
+function showForm() {
+    addTaskDropdown = new Dropdown({
+        events: {
+            onCreateEntryClick: () => {
+                tagsManager.showModal();
+                toggleCreateTagOption(true);
+            },
+        },
+        getEntryData: () => {
+            const currentTags = TagInterface.tags;
+
+            return currentTags.map((tag) => {
+                let data = {};
+
+                data.title = tag.title;
+                data.id = tag.id;
+
+                return data;
+            });
+        },
+        updateOnCreate: true,
+        type: "tag",
+        selectedEntriesPlaceholder: "No tags...",
+        ignoredElements: [tagsManager],
+    });
+
+    addTaskForm.insertBefore(
+        addTaskDropdown.container,
+        addTaskForm.lastElementChild,
+    );
+
+    const inputs = addTaskForm.querySelectorAll("input, textarea, select");
+
+    inputs.forEach((input) => {
+        input.disabled = false;
+    });
+
+    showAddTaskButton.classList.add("activated");
+    addTaskDialog.classList.add("activated");
+}
+
+function closeForm() {
+    showAddTaskButton.classList.remove("activated");
+    addTaskDialog.classList.remove("activated");
+}
+
+function resetForm() {
+    const inputs = addTaskForm.querySelectorAll("input, textarea, select");
+
+    inputs.forEach((input) => {
+        input.disabled = true;
+    });
+
+    resetDropdown();
+
+    addTaskForm.reset();
+}
+
+function resetDropdown() {
+    if (addTaskDropdown) {
+        addTaskDropdown.remove();
+        addTaskDropdown = null;
+    }
+}
+
+function submitNewTask() {
+    const data = new FormData(addTaskForm);
+    const title = data.get("task-title");
+    const dueDate = data.get("task-date");
+    const priority = data.get("task-priority");
+
+    const task = TaskInterface.createTask(
+        title,
+        "Description",
+        new Date(dueDate),
+        Number(priority),
+    );
+
+    addTaskDropdown.selectedEntries.forEach((id) => {
+        task.addTag(id);
+    });
+
+    renderer.createTaskElementInDom(task);
+}
 
 submitNewTaskButton.addEventListener("click", () => {
     if (addTaskForm.checkValidity()) {
@@ -276,6 +254,26 @@ taskDateInput.addEventListener("click", () => {
 });
 
 // Loading save data and user interface
+function loadDefaultTags() {
+    TagInterface.createTag("Example Tag");
+}
+
+function loadDefaultTasks() {
+    let myTask = TaskInterface.createTask(
+        "Example task",
+        "Example description",
+        add(new Date(), {
+            days: 1,
+        }),
+        1,
+    );
+
+    myTask.addTag(TagInterface.tags[0].id);
+}
+
+function loadUI() {
+    renderer.createAllTaskElements();
+}
 
 if (localStorage.getItem("tags")) {
     TagInterface.loadSavedTags();
